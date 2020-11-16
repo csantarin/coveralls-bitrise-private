dde6541208cfe2dd754b3780dd5eeee465dcec1d
Cezar Andrew Villegas Santarin <csantarin@moneylion.com>
2020-11-16 12:33:46 +0800

    docs(notify): describe what each line does

---
+++ scripts/set_notify.sh
@@ -0,0 +1,7 @@
+# https://askubuntu.com/a/58828
+
+# To set variable only for current shell:
+BUILD_URL_1="https://www.google.com"
+
+# To set it for current shell and all processes started from current shell:
+export BUILD_URL_2="https://www.moneylion.com"
