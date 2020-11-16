5ea9ddb180e0a1316507ca93d392c1df424fc45b
Cezar Andrew Villegas Santarin <csantarin@moneylion.com>
2020-11-16 12:47:53 +0800

    ci(notify): try envman

--- scripts/set_notify.sh
+++ scripts/set_notify.sh
@@ -1,7 +1,11 @@
 # https://askubuntu.com/a/58828
 
 # To set variable only for current shell:
-BUILD_URL_1="https://www.google.com"
+BUILD_URL_KEY_1="https://www.google.com"
 
-# To set it for current shell and all processes started from current shell:
-export BUILD_URL_2="https://www.moneylion.com"
+# To set variable for current shell and all processes started from current shell:
+export BUILD_URL_KEY_2="https://www.moneylion.com"
+
+# To set variable using envman:
+BUILD_URL_VALUE_3="https://www.cnn.com"
+envman add --key BUILD_URL_KEY_3 --value $BUILD_URL_VALUE_3
