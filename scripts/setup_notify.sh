486494935e69def314f6e22f9aa52c463785ac54
Cezar Andrew Villegas Santarin <csantarin@moneylion.com>
2020-11-16 13:09:29 +0800

    fix(ci-notify): prefix env vars with $
    So that Bitrise might be able to pick them up for assignment later...
    It seems that Bitrise picks the envman-assigned key-value pair, though.
    Maybe the $ sign is necessary after all.

--- scripts/setup_notify.sh
+++ scripts/setup_notify.sh
@@ -2,10 +2,10 @@
 echo "Setting up notification target URLs..."
 
 # To set variable only for current shell:
-BUILD_URL_KEY_1="https://www.google.com"
+$BUILD_URL_KEY_1="https://www.google.com"
 
 # To set variable for current shell and all processes started from current shell:
-export BUILD_URL_KEY_2="https://www.moneylion.com"
+export $BUILD_URL_KEY_2="https://www.moneylion.com"
 
 # To set variable using envman:
 BUILD_URL_VALUE_3="https://www.cnn.com"
