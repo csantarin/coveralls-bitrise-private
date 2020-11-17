9ed9b33174582ba8b3305e5e12928d6445d5e83b
Cezar Andrew Villegas Santarin <csantarin@moneylion.com>
2020-11-17 16:42:28 +0800

    fix(notify): try multiple fixed keys

--- scripts/setup_notify.sh
+++ scripts/setup_notify.sh
@@ -8,5 +8,9 @@ $BUILD_URL_KEY_1="https://www.google.com"
 export $BUILD_URL_KEY_2="https://www.moneylion.com"
 
 # To set variable using envman:
-BUILD_URL_VALUE_3="https://app.testproject.io/#/reports/projects/141894/jobs/CJx6Bo_yCkmXJ0SupyXbbg/execution/q0awV21eUECZmfRJcabqxg?es=2020-11-09T07:26:56.287Z \n https://app.testproject.io/#/reports/projects/141894/jobs/FEb9iE_Sj0OXwf8GkuYcIQ/execution/qpg_neREV0mcidWtHJ01tQ?es=2020-11-09T07:27:06.600Z \n https://app.testproject.io/#/reports/projects/141894/jobs/UnzfRQ1ckkSmUV-UBTqfMA/execution/i4HNGPZKS0eGb2KFYsGu_A?es=2020-11-09T07:27:01.113Z"
+BUILD_URL_VALUE_1="https://app.testproject.io/#/reports/projects/141894/jobs/CJx6Bo_yCkmXJ0SupyXbbg/execution/q0awV21eUECZmfRJcabqxg?es=2020-11-09T07:26:56.287Z"
+BUILD_URL_VALUE_2="https://app.testproject.io/#/reports/projects/141894/jobs/FEb9iE_Sj0OXwf8GkuYcIQ/execution/qpg_neREV0mcidWtHJ01tQ?es=2020-11-09T07:27:06.600Z"
+BUILD_URL_VALUE_3="https://app.testproject.io/#/reports/projects/141894/jobs/UnzfRQ1ckkSmUV-UBTqfMA/execution/i4HNGPZKS0eGb2KFYsGu_A?es=2020-11-09T07:27:01.113Z"
+envman add --key BUILD_URL_KEY_1 --value $BUILD_URL_VALUE_1
+envman add --key BUILD_URL_KEY_2 --value $BUILD_URL_VALUE_2
 envman add --key BUILD_URL_KEY_3 --value $BUILD_URL_VALUE_3
